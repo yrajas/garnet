9cb92be15e4aa650cc10a2c3233866d8ed5ad32f
Yoganand Rajasekaran <yrajas@ntdev.microsoft.com>
2024-05-14 13:07:00 -0700

    Initial changes.

---
+++ GarnetLibFuzz/GarnetLibFuzz.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using Embedded.perftest;
+using Garnet.server;
+
+namespace GarnetLibFuzz
+{
+    public class GarnetFuzzTarget
+    {
+        private static readonly EmbeddedRespServer server;
+        //private static RespServerSession session;
+
+        static GarnetFuzzTarget()
+        {
+            server = new EmbeddedRespServer(new GarnetServerOptions());
+
+        }
+
+        private static int CalculateLen(ReadOnlySpan<byte> data)
+        {
+            int total = 0;
+            for (int i = 0; i < data.Length; i++) total += data[i];
+            return total;
+        }
+
+        public static void GarnetFuzz(ReadOnlySpan<byte> input)
+        {
+            //var c = CalculateLen(input);
+            //if (c == 100) throw new Exception("Invalid input");
+            //Console.WriteLine(c);
+            //using var session = server.GetRespSession();
+        }
+    }
+}
