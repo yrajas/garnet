5fc8910209b4d87ea678f184af5d1d069cbdf4ef
Yoganand Rajasekaran <yrajas@microsoft.com>
2024-07-12 16:56:44 -0700

    Initial draft

---
+++ playground/GarnetJSON/JsonCommands.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System.Buffers;
+using System.Diagnostics;
+using System.Text;
+using Garnet.server;
+using Tsavorite.core;
+
+namespace GarnetJSON
+{
+    class JsonSET : CustomObjectFunctions
+    {
+        public override bool CopyUpdater(ReadOnlyMemory<byte> key, ReadOnlySpan<byte> input, IGarnetObject oldValue, IGarnetObject newValue, ref (IMemoryOwner<byte>, int) output, ref RMWInfo rmwInfo) => UpdateJson(input, newValue);
+
+        public override bool InitialUpdater(ReadOnlyMemory<byte> key, ReadOnlySpan<byte> input, IGarnetObject jsonObject, ref (IMemoryOwner<byte>, int) output, ref RMWInfo rmwInfo) => UpdateJson(input, jsonObject);
+
+        public override bool InPlaceUpdater(ReadOnlyMemory<byte> key, ReadOnlySpan<byte> input, IGarnetObject value, ref (IMemoryOwner<byte>, int) output, ref RMWInfo rmwInfo) => UpdateJson(input, value);
+
+        public override bool NeedInitialUpdate(ReadOnlyMemory<byte> key, ReadOnlySpan<byte> input, ref (IMemoryOwner<byte>, int) output) => true;
+
+        public override bool Reader(ReadOnlyMemory<byte> key, ReadOnlySpan<byte> input, IGarnetObject value, ref (IMemoryOwner<byte>, int) output, ref ReadInfo readInfo) => throw new NotImplementedException();
+
+        private static bool UpdateJson(ReadOnlySpan<byte> input, IGarnetObject jsonObject)
+        {
+            Debug.Assert(jsonObject is JsonObject);
+
+            int offset = 0;
+            var path = CustomCommandUtils.GetNextArg(input, ref offset).ToString();
+            var value = CustomCommandUtils.GetNextArg(input, ref offset).ToString();
+
+            ((JsonObject)jsonObject).Set(path, value);
+            return true;
+        }
+    }
+
+    class JsonGET : CustomObjectFunctions
+    {
+        public override bool CopyUpdater(ReadOnlyMemory<byte> key, ReadOnlySpan<byte> input, IGarnetObject oldValue, IGarnetObject newValue, ref (IMemoryOwner<byte>, int) output, ref RMWInfo rmwInfo) => throw new NotImplementedException();
+        public override bool InitialUpdater(ReadOnlyMemory<byte> key, ReadOnlySpan<byte> input, IGarnetObject value, ref (IMemoryOwner<byte>, int) output, ref RMWInfo rmwInfo) => throw new NotImplementedException();
+        public override bool InPlaceUpdater(ReadOnlyMemory<byte> key, ReadOnlySpan<byte> input, IGarnetObject value, ref (IMemoryOwner<byte>, int) output, ref RMWInfo rmwInfo) => throw new NotImplementedException();
+        public override bool NeedInitialUpdate(ReadOnlyMemory<byte> key, ReadOnlySpan<byte> input, ref (IMemoryOwner<byte>, int) output) => throw new NotImplementedException();
+
+        public override bool Reader(ReadOnlyMemory<byte> key, ReadOnlySpan<byte> input, IGarnetObject value, ref (IMemoryOwner<byte>, int) output, ref ReadInfo readInfo)
+        {
+            Debug.Assert(value is JsonObject);
+
+            int offset = 0;
+            var path = CustomCommandUtils.GetNextArg(input, ref offset).ToString();
+
+            var result = ((JsonObject)value).Get(path);
+            CustomCommandUtils.WriteBulkString(ref output, Encoding.UTF8.GetBytes(result));
+            return true;
+        }
+    }
+}
---
+++ playground/GarnetJSON/JsonObject.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System.Buffers;
+using Garnet.server;
+using Newtonsoft.Json.Linq;
+
+namespace GarnetJSON
+{
+    public class JsonObjectFactory : CustomObjectFactory
+    {
+        public override CustomObjectBase Create(byte type)
+            => new JsonObject(type);
+
+        public override CustomObjectBase Deserialize(byte type, BinaryReader reader)
+            => new JsonObject(type, reader);
+    }
+
+    public class JsonObject : CustomObjectBase
+    {
+        //readonly Dictionary<string, object> dict;
+        readonly JObject jObject;
+
+        public JsonObject(byte type)
+            : base(type, 0, MemoryUtils.DictionaryOverhead)
+        {
+            jObject = new();
+            // TODO: update size
+        }
+
+        public JsonObject(byte type, BinaryReader reader)
+            : base(type, reader, MemoryUtils.DictionaryOverhead)
+        {
+            jObject = new(reader.ReadString());
+            // TODO: update size
+        }
+
+        public JsonObject(JsonObject obj)
+            : base(obj)
+        {
+            jObject = obj.jObject;
+        }
+
+        public override CustomObjectBase CloneObject() => new JsonObject(this);
+
+        public override void SerializeObject(BinaryWriter writer)
+        {
+            writer.Write(jObject.ToString());
+        }
+
+        public override void Dispose()
+        {
+        }
+
+        public override void Operate(byte subCommand, ReadOnlySpan<byte> input, ref (IMemoryOwner<byte>, int) output, out bool removeKey) => throw new NotImplementedException();
+
+        public override unsafe void Scan(long start, out List<byte[]> items, out long cursor, int count = 10, byte* pattern = null, int patternLength = 0) => throw new NotImplementedException();
+
+        public void Set(string path, string value) => jObject.SelectToken(path)?.Replace(value);
+
+        internal string Get(string path) => jObject.ToString();
+    }
+}
